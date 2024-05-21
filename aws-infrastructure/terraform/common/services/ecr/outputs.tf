cd6258698fcd37011e0fa0f1a3bc3a7cd6327683
Alegres <32520354+Alegres@users.noreply.github.com>
2024-05-21 14:56:41 +0200

    Update outputs.tf

--- aws-infrastructure/terraform/common/services/ecr/outputs.tf
+++ aws-infrastructure/terraform/common/services/ecr/outputs.tf
@@ -2,8 +2,3 @@ output "ecr_repository_url" {
   description = "The repo URL for backend ECR"
   value = module.ecr_backend.ecr_repository_url
 }
-
-output "ecr_monitoring_repository_url" {
-  description = "The repo URL for Monitoring ECR"
-  value = module.ecr_monitoring.ecr_repository_url
-}
