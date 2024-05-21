a19be56ae2bb6e810735622d7f95d0177f57aac0
Alegres <32520354+Alegres@users.noreply.github.com>
2024-05-21 21:26:11 +0200

    Update setup_new_region.sh

--- aws-infrastructure/terraform/setup_new_region.sh
+++ aws-infrastructure/terraform/setup_new_region.sh
@@ -1,6 +1,39 @@
 #!/usr/bin/env bash
 
 set -e
+
+TF_STATE_BUCKET="tf-state-${PROFILE}-${REGION}-${UNIQUE_BUCKET_STRING}"
+
+empty_tfstate_bucket() {
+  aws s3api delete-objects \
+      --bucket $TF_STATE_BUCKET \
+      --delete "$(aws s3api list-object-versions --bucket ${TF_STATE_BUCKET} --output=json --query='{Objects: Versions[].{Key:Key,VersionId:VersionId}}')" \
+      --profile $PROFILE \
+      --region $REGION || true
+}
+
+delete_secrets_manager() {
+  aws secretsmanager delete-secret \
+      --secret-id backend-secretsmanager-test-eu-central-1 \
+      --force-delete-without-recovery \
+    	--profile $PROFILE \
+    	--region $REGION || true
+}
+
+empty_ecr() {
+  ECR_REPOSITORY="backend"
+  aws ecr batch-delete-image \
+      --repository-name $ECR_REPOSITORY \
+      --profile $PROFILE \
+      --region $REGION \
+      --image-ids "$(aws ecr list-images --region $REGION --profile $PROFILE --repository-name $ECR_REPOSITORY --query 'imageIds[*]' --output json)" || true
+}
+
+delete_log_groups() {
+  aws logs describe-log-groups --query 'logGroups[*].logGroupName' --output table --region $REGION --profile $PROFILE | \
+  awk '{print $2}' | grep -v ^$ | while read x; do  echo "deleting $x" ; aws logs delete-log-group --log-group-name $x --region $REGION --profile $PROFILE; done || true
+}
+
 if [ "$#" -lt 4 ]; then
   echo "Not enough arguments provided."
   echo
@@ -42,18 +75,24 @@ HUB="${REGION_TO_HUB[$REGION]}"
 ACTION=${@:4}
 
 if [ "$ACTION" = "destroy -auto-approve" ]; then
-  ./$SCRIPT $PROFILE $REGION common/services/measurements-dynamodb $ACTION
-  delete_secrets_manager
-  ./$SCRIPT $PROFILE $REGION common/services/ecs-backend-service $ACTION
-  ./$SCRIPT $PROFILE $REGION common/services/ecs-backend-cluster $ACTION
-  ./$SCRIPT $PROFILE $REGION common/services/ecr $ACTION
-  ./$SCRIPT $PROFILE $REGION common/monitoring/sns $ACTION
-  ./$SCRIPT $PROFILE $REGION common/networking/securitygroups $ACTION
-  ./$SCRIPT $PROFILE $REGION common/networking/vpc $ACTION
-  ./$SCRIPT $PROFILE $REGION environments/$PROFILE/$HUB/$REGION/globals $ACTION
-  ./$SCRIPT $PROFILE $REGION common/general/dynamo-lock $ACTION.
-  empty_tfstate_bucket
-  ./$SCRIPT $PROFILE $REGION common/general/create-remote-state-bucket $ACTION
+  if aws s3api head-bucket --bucket $TF_STATE_BUCKET --profile $PROFILE --region $REGION 2>/dev/null; then
+    ./$SCRIPT $PROFILE $REGION common/services/measurements-dynamodb $ACTION
+    delete_secrets_manager
+    empty_ecr
+    ./$SCRIPT $PROFILE $REGION common/services/ecs-backend-service $ACTION
+    ./$SCRIPT $PROFILE $REGION common/services/ecs-backend-cluster $ACTION
+    ./$SCRIPT $PROFILE $REGION common/services/ecr $ACTION
+    ./$SCRIPT $PROFILE $REGION common/monitoring/sns $ACTION
+    ./$SCRIPT $PROFILE $REGION common/networking/securitygroups $ACTION
+    ./$SCRIPT $PROFILE $REGION common/networking/vpc $ACTION
+    ./$SCRIPT $PROFILE $REGION environments/$PROFILE/$HUB/$REGION/globals $ACTION
+    ./$SCRIPT $PROFILE $REGION common/general/dynamo-lock $ACTION
+    empty_tfstate_bucket
+    ./$SCRIPT $PROFILE $REGION common/general/create-remote-state-bucket $ACTION
+    delete_log_groups
+  else
+    echo "Skipping destroy - everything was already destroyed!"
+  fi
 else
   ./$SCRIPT $PROFILE $REGION common/general/create-remote-state-bucket $ACTION
   ./$SCRIPT $PROFILE $REGION common/general/dynamo-lock $ACTION
@@ -66,20 +105,3 @@ else
   ./$SCRIPT $PROFILE $REGION common/services/ecs-backend-service $ACTION
   ./$SCRIPT $PROFILE $REGION common/services/measurements-dynamodb $ACTION
 fi
-
-empty_tfstate_bucket() {
-  TF_STATE_BUCKET="tf-state-${PROFILE}-${REGION}-${UNIQUE_BUCKET_STRING}"
-  aws s3api delete-objects \
-      --bucket $TF_STATE_BUCKET \
-      --delete "$(aws s3api list-object-versions --bucket ${TF_STATE_BUCKET} --output=json --query='{Objects: Versions[].{Key:Key,VersionId:VersionId}}')" \
-      --profile $PROFILE \
-      --region $REGION
-}
-
-delete_secrets_manager() {
-  aws secretsmanager delete-secret \
-      --secret-id backend-secretsmanager-test-eu-central-1 \
-      --force-delete-without-recovery \
-    	--profile $PROFILE \
-    	--region $REGION
-}
